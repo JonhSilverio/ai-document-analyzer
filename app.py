f225a071bca861632976d907217d373c9f072002
JonhSilverio <johnsilveriosantossilva@gmail.com>
2026-02-21 23:55:25 -0300

    add gunicorn and fix port binding

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def analyze_document():
         return jsonify({"error": "Falha ao analisar o documento com IA."}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run()
