bd295c121b706a5f96df0353078d27fb066d8280
JonhSilverio <johnsilveriosantossilva@gmail.com>
2026-02-23 01:20:03 -0300

    🔧  adjust host port and python runtime.

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ def analyze_document():
 
 if __name__ == '__main__':
    
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
